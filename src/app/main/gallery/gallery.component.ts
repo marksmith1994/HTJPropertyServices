24a2c7a49db9eefdf7ed725afe6cee64da232d20
Mark Smith <mark.alex.smith@hotmail.co.uk>
2022-01-12 21:58:33 +0000

    #16 Fixed npm issues

--- src/app/main/gallery/gallery.component.ts
+++ src/app/main/gallery/gallery.component.ts
@@ -11,7 +11,7 @@ export class GalleryComponent implements OnInit {
 
 	title: string = "Gallery";
 	images: InstagramArray | undefined;
-	token: string = '';
+	token: string = 'IGQVJVenJ6S3hVeWtDWDl1aDl2NWdRMVZATdVFLdU9Qb3AtNnNkZAmV0bndvTmtkZAEdiTnZAqNVZA3VmllRExrUjlLU0V0enJ5UFhtUk5zRzloaGxGVThBYmNCQVdUQTBaS3JPWVNHb01zbXZA2ZAy1aYnlHMwZDZD';
 	loading: boolean = true;
     error: any = null;
 	
